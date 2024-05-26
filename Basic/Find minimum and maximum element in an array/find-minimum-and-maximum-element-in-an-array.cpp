c6c135b81a0096161696d23243ef5d58e8811d9a
ACHYUTKRCHAUDHARY <162984049+ACHYUTKRCHAUDHARY@users.noreply.github.com>
2024-05-26 22:49:53 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find minimum and maximum element in an array/find-minimum-and-maximum-element-in-an-array.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+public:
+    pair<long long, long long> getMinMax(long long a[], int n) {
+        // code here
+        long long int maxh=a[0],minh=a[0];
+        for(int i=0;i<n;i++){
+            minh=min(minh,a[i]);
+            maxh=max(maxh,a[i]);
+        }
+        return {minh,maxh};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(a, n);
+        
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
